f6abaccd6530d98a4217c5c3573ba1322bc7f20a
unknown <kororolucilfer@gmail.com>
2025-09-09 18:34:35 +0800

    REIGNE

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/reigne',   // 👈 use your repo name here
+  base: '/reigne/',   // 👈 use your repo name here
 })
